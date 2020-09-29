668c146939df521928984d76a412b07e2d651a05
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2020-09-29 16:51:44 +0530

    product edit form

--- src/app/product/components/product-edit/product-edit.component.ts
+++ src/app/product/components/product-edit/product-edit.component.ts
@@ -1,4 +1,9 @@
+import { ProductService } from './../../services/product.service';
+import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// create/ update existing record
 
 @Component({
   selector: 'app-product-edit',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-edit.component.scss']
 })
 export class ProductEditComponent implements OnInit {
+  // products/edit/10 - /:id (id ==> 10) 
 
-  constructor() { }
+  product: Product = new Product(); //
+
+  constructor(private route: ActivatedRoute, 
+              private router: Router, 
+              private productService: ProductService) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    console.log('id ', id)
+
+    if (id) {
+      this.productService.getProduct(id)
+                         .subscribe (product => {
+                           this.product = product;
+                         });
+    }
+  }
+
+  cancelEdit() {
+    this.router.navigate(['/', 'products']);
+    // this.router.navigateByUrl("/products")
+  }
+
+  saveProduct() {
+     this.productService.saveProduct(this.product)
+                        .subscribe( product => {
+                          console.log('product saved', product)
+                          this.product = product;
+                        });
+  }
+
+  deleteProduct() {
+    this.productService.deleteProduct(this.product.id)
+                       .subscribe( data => {
+                         this.router.navigateByUrl("/products")
+                       })
   }
 
 }
--- src/app/product/services/product.service.ts
+++ src/app/product/services/product.service.ts
@@ -1,7 +1,9 @@
+import { Observable } from 'rxjs';
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,35 @@ export class ProductService {
   constructor(private http: HttpClient) { 
   }
   
-  getProducts() {
+  getProducts():Observable<Product[]> {
     return this.http
-               .get(`${environment.apiEndPoint}/api/products`);
+               .get<Product[]>(`${environment.apiEndPoint}/api/products`);
   }
+
+  getProduct(id: number): Observable<Product> {
+    return this.http
+    .get<Product>(`${environment.apiEndPoint}/api/products/${id}`);
+  }
+
+  // returns data saved/created in the db
+  saveProduct(product: Product): Observable<Product> {
+    // if product.id then edit else create
+    // update - put method
+    // to create - post method
+    if (product.id) {
+      // update
+      return this.http
+                 .put<Product>(`${environment.apiEndPoint}/api/products/${product.id}`,product)
+    } else {
+      // create 
+      return this.http
+                 .post<Product>(`${environment.apiEndPoint}/api/products`,product)
+    }
+  }
+
+  deleteProduct(id: number) {
+    return this.http
+    .delete<any>(`${environment.apiEndPoint}/api/products/${id}`);
+  }
+
 }
